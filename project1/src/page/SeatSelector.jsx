d8301a0d7642e7070c96419e0faa33acf420cfd6
iamtheshru <s.j.patel.3110@gmail.com>
2023-12-12 17:54:04 +0530

    data

--- project1/src/page/SeatSelector.jsx
+++ project1/src/page/SeatSelector.jsx
@@ -1,34 +1,40 @@
 import React, { useEffect, useState } from "react";
+import './customcss.css';
+
 
 // Dummy JSON data for seats
 const seatsData = [
-    { id: 1, seatName: "A1", booked: false },
-    { id: 2, seatName: "A2", booked: true },
-    { id: 3, seatName: "A3", booked: false },
-    { id: 4, seatName: "A4", booked: false },
-    { id: 5, seatName: "A5", booked: false },
-    { id: 6, seatName: "A6", booked: false },
-    { id: 7, seatName: "A7", booked: false },
-    { id: 8, seatName: "A8", booked: false },
-    { id: 9, seatName: "A9", booked: false },
-    { id: 10, seatName: "A10", booked: false },
-    { id: 11, seatName: "A11", booked: false },
-    { id: 12, seatName: "A12", booked: false },
-    { id: 13, seatName: "A13", booked: false }
-    { id: 14, seatName: "A14", booked: false },
-    { id: 15, seatName: "A15", booked: false },
-    { id: 16, seatName: "A16", booked: false },
-    { id: 17, seatName: "A17", booked: false },
-    { id: 18, seatName: "A18", booked: false },
-    { id: 19, seatName: "A19", booked: false }
+    { id: 1, seatNumber: "A1", booked: false },
+    { id: 2, seatNumber: "A2", booked: false },
+    { id: 3, seatNumber: "A3", booked: false },
+    { id: 4, seatNumber: "A4", booked: false },
+    { id: 5, seatNumber: "A5", booked: false },
+    { id: 6, seatNumber: "A6", booked: false },
+    { id: 7, seatNumber: "A7", booked: false },
+    { id: 8, seatNumber: "A8", booked: false },
+    { id: 9, seatNumber: "A9", booked: false },
+    { id: 10, seatNumber: "A10", booked: false },
+    { id: 11, seatNumber: "A11", booked: false },
+    { id: 12, seatNumber: "A12", booked: false },
+    { id: 13, seatNumber: "A13", booked: false },
+    { id: 14, seatNumber: "A14", booked: false },
+    { id: 15, seatNumber: "A15", booked: false },
+    { id: 16, seatNumber: "A16", booked: false },
+    { id: 17, seatNumber: "A17", booked: false },
+    { id: 18, seatNumber: "A18", booked: false },
+    { id: 19, seatNumber: "A19", booked: false }
     // Add more seat objects as needed
 
 ]
 
 const SeatSelector = () => {
+    const [seats, setSeats] = useState(seatsData);
     const [numSeats, setNumSeats] = useState(0);
     const [selectedSeats, setSelectedSeats] = useState([]);
-    const [seats, setSeats] = useState(seatsData);
+    const [book, setBook] = useState(false);
+    const [confirmed, setConfirmed] = useState(false);
+    const [message, setMessage] = useState('');
+
 
     useEffect(() => {
         let storedSeatData = seatsData;
@@ -39,41 +45,93 @@ const SeatSelector = () => {
 
     const handleInputChange = (e) => {
         setNumSeats(parseInt(e.target.value))
+    }
+    const handleInputSubmit = (e) => {
+        e.preventDefault();
         // Show seat selection page
         // You can implement routing or toggle a state to switch the view
     }
-    const handleInputSubmit = (e) => {
-        e.preventDefault()
+
+    const confirmBtn = (seatId) => {
+        const updatedSeats = seats.map(seat => {
+            if (seat.id === seatId && !seat.booked && selectedSeats.length < numSeats) {
+                return { ...seat, booked: !seat.booked };
+            }
+            return seat;
+
+        });
+        const updatedSelectedSeats = updatedSeats.filter(seat => seat.booked).map(seat => seat.id);
+        setSelectedSeats(updatedSelectedSeats);
+        setSeats(updatedSeats);
+        if (numSeats > 0 && updatedSelectedSeats.length > 0) {
+            setBook(true)
+            setNumSeats(0)
+        } else {
+            setBook(false)
+        }
     }
+
+    const toggleSeatSelection = (seatId) => {
+        console.log(seatId);
+        const updatedSeats = seats.map(seat => {
+            console.log(seat.id);
+            if (seat.id === seatId && !seat.booked && selectedSeats.length < numSeats) {
+
+                return { ...seat, booked: !seat.booked }
+            }
+            return seat;
+        });
+
+        const updatedSelectedSeats = updatedSeats.filter(seat => seat.booked).map(seat => seat.id);
+        setSelectedSeats(updatedSelectedSeats);
+        setSeats(updatedSeats);
+
+        localStorage.setItem("seatsData", JSON.stringify(updatedSeats));
+    }
+
+    const handleChangeYes = () => {
+        setBook(false);
+        setConfirmed(true)
+        setMessage('ticket booked succesfully')
+    }
+
+
+
 
     return (<>
         <div className="container">
             <h2>Book Now</h2>
             <div className="row">
-                <div className="col">
+                <div className="col mt-5">
                     <form onSubmit={handleInputSubmit}>
                         <label htmlFor="numSeats">Number of Seats:</label>
                         <input type="number" id="numSeats" name="numSeats" value={numSeats} onChange={handleInputChange} />
-                        <button type="submit">Book Seats</button>
+                        <button type="submit" onClick={confirmBtn}>Book Seats</button>
                     </form>
                 </div>
             </div>
             <div className="row mt-4">
-                <div className="col-4">
+                <div className="col-4" style={{ columns: 3, columnGap: "20px" }}>
                     {/* {JSON.stringify(seats)} */}
                     <div className="seatMap">
                         {seats.map(seat =>
                             <div key={seat.id}
                                 className={`seat ${seat.booked ? "booked" : (selectedSeats.includes(seat.id) ? "selected" : "available")}`}
-                                onClick={() => toggleSeatSelection(seat.id)}></div>
+                                style={{ backgroundColor: confirmed && seat.booked ? "green" : "" }}
+                                onClick={() => toggleSeatSelection(seat.id)}>
+                                {seat.seatNumber}
+                            </div>
                         )}
                     </div>
                 </div>
             </div>
-        </div>
-
-
+            {book && (<div>
+                <p>Ticket confirmation message</p>
+                <button onClick={handleChangeYes}>Yes</button>
+            </div>)}
+            <p>{message}</p>
+        </div >
     </>);
 }
 
-export default SeatSelector;
+export default SeatSelector
