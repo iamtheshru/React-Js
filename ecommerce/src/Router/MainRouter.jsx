8aaecc9a58aa91d0d2bbf7f69f1ee05acd6fa495
iamtheshru <s.j.patel.3110@gmail.com>
2023-12-18 17:56:18 +0530

    data

--- ecommerce/src/Router/MainRouter.jsx
+++ ecommerce/src/Router/MainRouter.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Header from "./../Componente/Header.jsx";
+import { createBrowserRouter } from "react-router-dom";
 
-const aa = () => {
-    return (<>
-        <Header />
-    </>);
-}
+const MainRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: (
+            <>
+                <Header />
+            </>
+        )
+    }
+])
 
-export default aa;
+
+export default MainRouter;
