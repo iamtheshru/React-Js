2506ad43c52bb15830007d3da1f16eb26fba1fd6
iamtheshru <s.j.patel.3110@gmail.com>
2023-12-21 17:57:50 +0530

    data

--- ecommerce/src/Router/MainRouter.jsx
+++ ecommerce/src/Router/MainRouter.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import Header from "./../Componente/Header.jsx";
+import SpecialOffers from "./../Componente/SpecialOffers.jsx";
+import AllProducts from "./../Pages/AllProducts.jsx";
+import ChairData from "./../Pages/ChairData.jsx";
+import SofaData from "./../Pages/SofaData.jsx";
+import StoolData from "./../Pages/StoolData.jsx";
+import FootstoolData from "./../Pages/FootstoolData.jsx";
+import OfficeData from "./../Pages/OfficeData.jsx";
 import { createBrowserRouter } from "react-router-dom";
 
 const MainRouter = createBrowserRouter([
@@ -8,6 +15,61 @@ const MainRouter = createBrowserRouter([
         element: (
             <>
                 <Header />
+                <SpecialOffers />
+            </>
+        )
+    },
+    {
+        path: '/allproduct',
+        element: (
+            <>
+                <Header />
+                <AllProducts />
+            </>
+        )
+    },
+    {
+        path: '/chair',
+        element: (
+            <>
+                <Header />
+                <ChairData />
+            </>
+        )
+    },
+    {
+        path: '/sofa',
+        element: (
+            <>
+                <Header />
+                <SofaData />
+            </>
+        )
+    },
+    {
+        path: '/stool',
+        element: (
+            <>
+                <Header />
+                <StoolData />
+            </>
+        )
+    },
+    {
+        path: '/footstool',
+        element: (
+            <>
+                <Header />
+                <FootstoolData />
+            </>
+        )
+    },
+    {
+        path: '/office',
+        element: (
+            <>
+                <Header />
+                <OfficeData />
             </>
         )
     }
